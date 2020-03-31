3740701d49927ba2d91b1d7eb6f3381c54df0880
danielbarcenas <barcenas.proteco@gmail.com>
2020-03-31 04:54:30 -0600

    View(Module: Login)

---
+++ ChatFirebase/app/src/main/java/com/example/chatfirebase/mainModule/MainActivity2.java
@@ -0,0 +1,16 @@
+package com.example.chatfirebase.mainModule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chatfirebase.R;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
