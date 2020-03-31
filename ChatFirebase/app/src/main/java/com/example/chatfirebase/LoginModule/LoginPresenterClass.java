bdec4976b4080f6dbfe1fe9c6289af05c6517fca
danielbarcenas <barcenas.proteco@gmail.com>
2020-03-31 04:36:44 -0600

    Presenter(Module: Login)

---
+++ ChatFirebase/app/src/main/java/com/example/chatfirebase/LoginModule/LoginPresenterClass.java
@@ -0,0 +1,107 @@
+package com.example.chatfirebase.LoginModule;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.example.chatfirebase.LoginModule.events.LoginEvent;
+import com.example.chatfirebase.LoginModule.model.LoginInteractor;
+import com.example.chatfirebase.LoginModule.model.LoginInteractorClass;
+import com.example.chatfirebase.LoginModule.view.LoginView;
+import com.example.chatfirebase.LoginModule.view.MainActivity;
+import com.example.chatfirebase.R;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+public class LoginPresenterClass implements LoginPresenter {
+
+    private LoginView mView;
+    private LoginInteractor mInteractor;
+
+    public LoginPresenterClass(LoginView mView) {
+        this.mView = mView;
+        this.mInteractor = new LoginInteractorClass();
+    }
+
+
+
+    @Override
+    public void OnCreate() {
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void OnResume() {
+        if(setProgress()){
+            mInteractor.OnResume();
+        }
+
+    }
+
+    private boolean setProgress() {
+        if(mView != null){
+            mView.showProgress();
+            return  true;
+        }
+        return  false;
+    }
+
+    @Override
+    public void OnPause() {
+        if(setProgress()){
+            mInteractor.OnPause();
+        }
+    }
+
+    @Override
+    public void OnDestroy() {
+        mView = null;
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    public void result(int requestCode, int resultCode, Intent data) {
+        if(resultCode == Activity.RESULT_OK){
+            switch (requestCode){
+                case MainActivity.RC_SIGN_IN:
+                    if(data != null){
+                        mView.showLoginSucessfully(data);
+                    }
+                    break;
+            }
+        }else{
+            mView.showError(R.string.login_message_error);
+        }
+    }
+
+    @Override
+    public void getStatusAuth() {
+        if(setProgress()){
+            mInteractor.getStatusAuth();
+        }
+
+    }
+
+    @Subscribe
+    @Override
+    public void onEventListener(LoginEvent event) {
+        if(mView != null){
+            mView.hideProgress();
+
+            switch (event.getTypeEvent()){
+                case LoginEvent.STATUS_AUTH_SUCCESS:
+                    if (setProgress()){
+                        mView.showMessageStarting();
+                        mView.openMainActivity();
+                    }
+                    break;
+                case  LoginEvent.STATUS_AUTH_ERROR:
+                    mView.openUILogin();
+                    break;
+                case LoginEvent.ERROR_SERVER:
+                    mView.showError(event.getResMsg());
+                    break;
+            }
+        }
+    }
+}
