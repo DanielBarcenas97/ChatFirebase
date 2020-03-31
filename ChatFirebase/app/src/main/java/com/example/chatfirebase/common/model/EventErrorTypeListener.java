b498469aa4f429ec927cbfdfa030224a6bed0df3
danielbarcenas <barcenas.proteco@gmail.com>
2020-03-31 03:06:27 -0600

    DataAccess: Realtime Database(Login)

---
+++ ChatFirebase/app/src/main/java/com/example/chatfirebase/common/model/EventErrorTypeListener.java
@@ -0,0 +1,6 @@
+package com.example.chatfirebase.common.model;
+
+public interface EventErrorTypeListener {
+
+    void onError(int typeEvent,int resMsg);
+}
---
+++ ChatFirebase/app/src/main/java/com/example/chatfirebase/common/model/dataAccess/FirebaseRealtimeDatabaseAPI.java
@@ -0,0 +1,46 @@
+package com.example.chatfirebase.common.model.dataAccess;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FirebaseRealtimeDatabaseAPI {
+
+    public static final String SEPARATOR = "___&___";
+
+    public static final String PATH_USERS = "users";
+
+    public static final String PATH_CONTACTS = "contacts";
+
+    public static final String PATH_REQUEST = "requests";
+
+    private DatabaseReference mDatabaseReference;
+
+    private static class SinglentonHolder{
+       private static final FirebaseRealtimeDatabaseAPI INSTANCE = new FirebaseRealtimeDatabaseAPI();
+    }
+
+    public static FirebaseRealtimeDatabaseAPI getInstance() {
+        return SinglentonHolder.INSTANCE;
+    }
+
+    private FirebaseRealtimeDatabaseAPI(){
+
+        this.mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+    }
+
+
+    /*
+    References
+     */
+
+
+    public DatabaseReference getRootReference(){
+        return mDatabaseReference.getRoot();
+    }
+
+    public  DatabaseReference getUserReferenceByUid(String uid){
+        return getRootReference().child(PATH_USERS).child(uid);
+
+    }
+
+}
